87297098cd956c23c3d281ead06408315751b4c5
parishar <parishar@iu.edu>
2024-12-08 00:46:06 -0500

    Pushing files for Assignment11

---
+++ Assignment11/a11_optional.py
@@ -0,0 +1,53 @@
+import random as rn
+
+
+#problem 1
+#input list of semi-ordered intervals
+#output combined intervals under conditions
+def combine(lst):
+    pass
+
+
+#problem 2
+def b(lst):
+    pass
+
+
+
+#problem 3
+
+#defined the homework
+def star(n):
+    pass
+
+#defined in homework    
+def star_star(n):
+    pass
+
+
+
+if __name__ == "__main__":
+
+    #problem 1
+    # lst = [[[1,3]],
+    #     [[1,3],[4,5]],
+    #     [[1,3],[2,6],[8,11],[9,12]],
+    #     [[1,2],[2,3],[3,4],[4,12]],
+    #     [[1,2],[3,4],[5,6]]]
+
+    # for el in lst:
+    #     print(f"{el} → {combine(el)}")
+        
+    #problem 2
+    # data = [[1,2,3],[0,1,1,0,1,1],[0],[3,3],[1,2,3,3,2,1],[1,2,3,3,2,6]]
+
+    # for d in data:
+    #     ptr = b(d)
+    #     print(ptr, d[0:ptr],d[ptr:],sum(d[0:ptr])-sum(d[ptr:]))
+
+    #problem 3
+    # for i in range(10):
+    #     print(i, star_star(i))
+
+
+    print()
