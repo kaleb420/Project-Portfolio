4d0b44d44af16d98c81e3b2bd252d693cece3b25
Kaleb Jonathan Robinson <kalerobi@silo.luddy.indiana.edu>
2025-05-21 00:55:35 -0400

    A1-11

--- assignment1/a1problem10.c
+++ assignment1/a1problem10.c
@@ -1,10 +1,42 @@
 #include <stdio.h>
 int main(void){
-	int code;
-	char *digits;
-	char *encrypted;
-	int i=0;
-	printf("Enter a four digit integer: ");
-	scanf("%s", &code);
-	
+	int odigit;
+	int tdigit;
+	int thdigit;
+	int fdigit;
+	int encrypted;
+	int decrypted;
+	char type;
+	scanf("%1d%1d%1d%1d", &odigit, &tdigit, &thdigit, &fdigit);
+		encrypted=(((odigit+7)%10)*10)+((tdigit+7)%10)+(((thdigit+7)%10)*1000)+(((fdigit+7)%10)*100);
+		decrypted=(((odigit+13)%10)*10)+((tdigit+13)%10)+(((thdigit+13)%10)*1000)+(((fdigit+13)%10)*100);
+	scanf(" %c", &type);
+	if (type=='e'){
+		if (encrypted>1000){
+			printf("%d", encrypted);
+		}
+		else if(encrypted<1000){
+			printf("0%d", encrypted);
+		}
+		else if(encrypted<100){
+			printf("00%d", encrypted);
+		}
+		else if(encrypted<10){
+			printf("000%d", encrypted);
+		}
+	}
+	else if(type=='d'){
+		if (encrypted>1000){
+			printf("%d", decrypted);
+		}
+		else if(encrypted<1000){
+			printf("0%d", decrypted);
+		}
+		else if(encrypted<100){
+			printf("00%d", decrypted);
+		}
+		else if(encrypted<10){
+			printf("000%d", decrypted);
+		}
+	}
 }
