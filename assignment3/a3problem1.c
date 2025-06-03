ff4b992d82ed9c7002ec3ada6cfe5de871bccdb3
Kaleb Jonathan Robinson <kalerobi@silo.luddy.indiana.edu>
2025-06-03 19:54:57 -0400

    A3-5

--- assignment3/a3problem1.c
+++ assignment3/a3problem1.c
@@ -2,23 +2,13 @@
 void mergeArrays(int A[], int size_a, int B[], int size_b, int C[]){
 	int a_index=0, b_index=0, size_c=size_a+size_b;
 	for (int k=0; k<size_c; k++){
-		while (a_index<size_a || b_index<size_b){
-			if (A[a_index]>=B[b_index] && a_index<size_a){
-				C[k]=A[a_index];
-				a_index++;
-			}
-			else if (A[a_index]<B[b_index] && b_index<size_b){
-				C[k]=B[b_index];
-				b_index++;
-			}
-			else if (a_index<size_a){
-				C[k]=A[a_index];
-				a_index++;
-			}
-			else if (b_index<size_b){
-				C[k]=A[b_index];
-				b_index++;
-			}
+		if (A[a_index]>=B[b_index]){
+			C[k]=A[a_index];
+			a_index++;
+		}
+		else if (A[a_index]<B[b_index]){
+			C[k]=B[b_index];
+			b_index++;
 		}
 	}
 	printf("%ls", C);
--- assignment3/a3problem4.c
+++ assignment3/a3problem4.c
@@ -27,12 +27,12 @@ int findUnion(int *input1, int input1_length, int *input2, int input2_length){
 	int *temp1=input1;
 	int *temp2=input2;
 	for (int i=0; i<input1_length; i++){
-		for (int k=0; k<input1_length; i++){
+		for (int k=0; k<input1_length; k++){
 			input1=temp1;
 			inputone=temp1;
 			input1+=i;
 			inputone+=k;
-			if (i!=k && input1!=inputone)
+			if (i!=k && *input1!=*inputone)
 				different_elements++;
 		}
 		for (int j=0; j<input2_length; j++){
