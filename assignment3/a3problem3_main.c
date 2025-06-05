7227e603dd860d8224270a0dc7e98fc10421e469
Kaleb Jonathan Robinson <kalerobi@silo.luddy.indiana.edu>
2025-06-05 16:14:11 -0400

    A3-8

--- assignment3/a3problem3_main.c
+++ assignment3/a3problem3_main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include "a3problem3.h"
 int main(void){
-	char input[]="because";
-	int k=5;
-	//scanf("%s", str);
-	//scanf("%d", &k);
+	char input[20];;
+	int k;
+	scanf("%s", input);
+	scanf("%d", &k);
 	int le=length(input);
 	int n=le-k+1;
 	char kGrams[n][k+1];
