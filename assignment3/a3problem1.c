10d39c05638fdd695da4ebeb35fb583b017c29ab
Kaleb Jonathan Robinson <kalerobi@silo.luddy.indiana.edu>
2025-06-05 02:29:48 -0400

    A3-7

--- assignment3/a3problem1.c
+++ assignment3/a3problem1.c
@@ -2,14 +2,24 @@
 void mergeArrays(int A[], int size_a, int B[], int size_b, int C[]){
 	int a_index=0, b_index=0, size_c=size_a+size_b;
 	for (int k=0; k<size_c; k++){
-		if (A[a_index]>=B[b_index]){
+		if (A[a_index]>=B[b_index] && b_index<size_b){
 			C[k]=B[b_index];
 			b_index++;
 		}
-		else if (A[a_index]<B[b_index]){
+		else if (A[a_index]<B[b_index] && a_index<size_a){
+			C[k]=A[a_index];
+			a_index++;
+		} 
+		else if (a_index<size_a){
 			C[k]=A[a_index];
 			a_index++;
 		}
+		else if (b_index<size_b){
+			C[k]=B[b_index];
+			b_index++;
+		}
 	}
-	printf("%ls", C);
+	for (int i=0; i<size_a+size_b; i++){
+		printf("%d\n", C[i]);
+	}
 }
