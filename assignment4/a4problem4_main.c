a38c751f362307945f605d4511e63716f7fe160c
Kaleb Jonathan Robinson <kalerobi@silo.luddy.indiana.edu>
2025-06-09 17:07:49 -0400

    A4-2

--- assignment4/a4problem4_main.c
+++ assignment4/a4problem4_main.c
@@ -2,8 +2,5 @@
 #include "a4problem4.h"
 int main(void){
 	char *s="<aaa>bbb</aaa>";
-	char **strings=getXMLTagAndContent(s);
-	for (int i=0; i<3; i++){
-		printf("%s", strings[i]);
-	}
+	getXMLTagAndContent(s);
 }
