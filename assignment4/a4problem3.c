686aa5dbf8f4ec006ceb944ede0a23dfc80f7708
Kaleb Jonathan Robinson <kalerobi@silo.luddy.indiana.edu>
2025-06-10 01:05:17 -0400

    A4-3

---
+++ assignment4/a4problem3.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+long **read_content(void){
+	long **arr=(long **)malloc(sizeof(long *)*2);
+	long *index0;
+	long *index1=(long *)malloc(sizeof(long)*4);
+	long number=0;
+	long *integers=(long *)malloc(sizeof(long)*20);
+	long sum=0;
+	long average=0;
+	long range=0;
+	arr[0]=index0;
+	arr[1]=index1;
+	index1[0]=integers;
+	index1[1]=sum;
+	index1[2]=average;
+	index1[3]=range;
+	int smallest=10000000;
+	int largest=-10000000;
+	while (index0[number]!='\0'){
+		number+=1;
+		sum+=index0[number];
+		if (index0[number]>largest)
+			largest=index0[number];
+		if (index0[number]<smallest)
+			smallest=index0[number];
+	}
+	average=sum/(number+1);
+	range=largest-smallest;
+	free(arr);
+	free(index1);
+	free(integers);
+	printf("will return");
+	fflush(stdout);
+	return arr;
+}
+int main(void){
+	long **read=read_content();
+	printf("The array contains: [%ld]\n", read[0][0]);
+	printf("There are %ld numbers,\n", read[1][0]);
+	printf("The sum is %ld,\n", read[1][1]);
+	printf("The average is %ld,\n", read[1][2]);
+	printf("The range is %ld.", read[1][3]);
+}
