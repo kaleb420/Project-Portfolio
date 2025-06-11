66ef891e8470a899ad440bc5d5a4a0f041a77b58
Kaleb Jonathan Robinson <kalerobi@silo.luddy.indiana.edu>
2025-06-10 20:30:20 -0400

    A4-6

--- assignment4/a4problem2_main.c
+++ assignment4/a4problem2_main.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 #include "a4problem2.h"
 int main(void){
-	char *s="hello world, how are, you doing?";
+	char *s=",hello world, how are, you doing?,";
 	char d=',';
-	int n=1;
+	int n=0;
 	char **token=tokenize(s, d, &n);
+	n-=98;
 	for (int i=0; i<n; i++){
 		printf("%s", token[i]);
 	}
